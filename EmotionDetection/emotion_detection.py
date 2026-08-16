c67c823279ca390177a1cfc513b3f2a8963394e4
urosmitra <urke96m@gmail.com>
2026-08-15 21:27:54 -0400

    Package emotion detection app

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json=myobj, headers=headers)
+
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']
+
+    anger_score = emotion_scores['anger']
+    disgust_score = emotion_scores['disgust']
+    fear_score = emotion_scores['fear']
+    joy_score = emotion_scores['joy']
+    sadness_score = emotion_scores['sadness']
+
+    emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
